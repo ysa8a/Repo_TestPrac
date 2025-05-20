4e5c524082069959f65bdd8a9ac2e9f1f5ac0c29
ysa8a <ysa8a111@gmail.com>
2025-05-20 17:01:15 +0200

    Refactorizar la inicialización de la aplicación Flask y mover la creación de la app al bloque principal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from server import app
+from server import create_app
 from routes import auth, companies, companies_admin, users_admin
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
