1b19836f08bdbcfef2d7cabef361b5842b097fe6
ysa8a <ysa8a111@gmail.com>
2025-05-20 16:27:32 +0200

    Refactorizar la inicialización de la aplicación Flask en la función create_app()
    Signed-off-by: ysa8a <ysa8a111@gmail.com>

--- server.py
+++ server.py
@@ -7,12 +7,22 @@ from flask_wtf import CSRFProtect
 load_dotenv()
 # Set the Flask app configuration
 
-app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')  # Cargada de forma segura
-app.permanent_session_lifetime = 99999999
-csrf = CSRFProtect(app)
-app.config.update(
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SECURE=True,  # ✅ Solo si estás usando HTTPS
-    SESSION_COOKIE_SAMESITE='Lax'
-)
+def create_app():
+    """
+    Create and configure the Flask application.
+    """
+    # Load environment variables
+    load_dotenv()
+
+    # Initialize the Flask app
+    app = Flask(__name__)
+    app.secret_key = os.getenv('SECRET_KEY')  # Cargada de forma segura
+    app.permanent_session_lifetime = 99999999
+    csrf = CSRFProtect(app)
+    app.config.update(
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SECURE=True,  # ✅ Solo si estás usando HTTPS
+        SESSION_COOKIE_SAMESITE='Lax'
+    )
+
+    return app
